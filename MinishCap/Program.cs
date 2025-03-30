b9cd95568ed0a5efaf02b25730deab35f4ea41a0
Crumbler <25861574+Crumbler@users.noreply.github.com>
2025-03-30 17:51:39 +0300

    Added Silk

--- MinishCap/Program.cs
+++ MinishCap/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace MinishCap; 
+﻿using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+namespace MinishCap; 
 
 public static class Program {
+    private static IWindow _window = null!;
+
     public static void Main() {
         Console.WriteLine("Hello, World!");
 
+        var options = WindowOptions.Default with {
+            Size = new Vector2D<int>(800, 600),
+            Title = "Minish Cap",
+            FramesPerSecond = 60
+        };
 
+        _window = Window.Create(options);
+
+        _window.Run();
     }
 }
